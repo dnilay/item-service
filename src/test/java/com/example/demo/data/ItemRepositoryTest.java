df0315af3ead3966d06392f0470b3ddc719fd351
Nilay Das <mailnilaydas@gmail.com>
2021-04-23 14:23:18 +0530

    first commit

---
+++ src/test/java/com/example/demo/data/ItemRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import com.example.demo.model.Item;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class ItemRepositoryTest {
+	
+	@Autowired
+	private ItemRepository repository;
+	
+	@Test
+	public void testFindAll() {
+		List<Item> items = repository.findAll();
+		assertEquals(3,items.size());
+	}
+
+	@Test
+	public void testFindOne() {
+		Item item = repository.findById(10001).get();
+		
+		assertEquals("Item1",item.getName());
+	}
+
+}
